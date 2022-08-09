bc7b264ea96aff3e8ca79624a55e4500780b047c
Thadeu Alonso <thadeu.santos@gazeus.com>
2022-08-09 15:48:25 -0300

    Removing debug hello world

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,12 +3,8 @@
 int main()
 {
 	util::Platform platform;
-
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
-
 	sf::RenderWindow window;
+
 	// in Windows at least, this must be called before creating the window
 	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
 	// Use the screenScalingFactor
